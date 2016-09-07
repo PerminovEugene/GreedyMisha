0311b4246a23b33f59325b92e0afae11e6d8f3a1
eperminov <e.perminov@velvetech.com>
2016-09-07 20:16:48 +0600

    Finish algorithm. Need fix bug with maximum elements.

---
+++ greedyMishaAlgorithm.js
@@ -0,0 +1,70 @@
+var GreedyMishaAlgorithm = function () {};
+GreedyMishaAlgorithm.prototype.start = function(inputArray) {
+    try {
+        var minimalNumber = stringToDecimal(inputArray[0]);
+        var binarySum = decimalToBinary(minimalNumber);
+        var summWithoutMinimal = 0;
+        var item = 0;
+        for (var i = 1; i < inputArray.length; i++) {
+            item = stringToDecimal(inputArray[i]);
+            binarySum = binarySum ^ decimalToBinary(item);
+            minimalNumber = getMinimalNumber(minimalNumber, inputArray[i]);
+            if (minimalNumber > item) {
+                summWithoutMinimal += minimalNumber;
+                minimalNumber = item;
+            } else {
+                summWithoutMinimal += item
+            }
+        }
+        if (binarySum === 0) {
+            return summWithoutMinimal;
+        } else {
+            return false;
+        }
+    } catch (ex) {
+        throw ex;
+    }
+};
+var stringToDecimal = function(string) {
+    try {
+        var number = parseInt(string, 10)
+        if (isNaN(number)) {
+            throw "All elements must be numbers!"
+        }
+        return number;
+    } catch (ex) {
+        throw ex;
+    }
+};
+var decimalToBinary = function (number) {
+    return number.toString(2)
+};
+var getMinimalNumber = function(a, b) {
+    try {
+        var newA = parseInt(a, 10);
+        var newB = parseInt(b, 10);
+        if (isNaN(newA) || isNaN(newB)) {
+            throw "All elements must be numbers!"
+        }
+        return newA < newB ? newA : newB;;
+    } catch (ex) {
+        throw ex;
+    }
+};
+// TODO move it to first cycle.
+var findSumWithoutMinimalElement = function (array, minimalNumber) {
+    var sum = 0;
+    var minimalNumberPassed = false;
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] !== minimalNumber) {
+            if (minimalNumberPassed == false) {
+                sum += parseInt(array[i]);
+            }
+        } else {
+            minimalNumberPassed = true;
+        }
+    }
+    return sum;
+};
+
+module.exports.GreedyMishaAlgorithm = GreedyMishaAlgorithm;
---
+++ start.js
@@ -0,0 +1,94 @@
+var fs = require('fs');
+var greedyMishaAlgorithm = require('./greedyMishaAlgorithm');
+
+
+var NUMBER_OF_TESTS_STRING_INDEX = 0;
+var STARTED_OF_TESTS_STRING_INDEX = 1;
+var LINES_FOR_ONE_TEST_CASE = 2;
+var RESULT_BASIC_STRING = "Case #";
+var ENCODING = "utf8";
+var DEFAULT_FILE_IN_PATH = "./in.txt";
+var DEFAULT_FILE_OUT_PATH = "./out.txt";
+
+
+function readFromFile(filePath, encoding) {
+    return fs.readFileSync(filePath, encoding);
+}
+function writeToFile(string, filePath) {
+    fs.writeFileSync(filePath, string);
+}
+
+function preparationOfInputData(array) {
+    var preparedArray = [];
+    for (var i=0; i< array.length; i++) {
+        preparedArray.push(array[i].replace("\r", ""))
+    }
+    return preparedArray;
+}
+
+function prepareInputArrayForAlgorithm(length, array) {
+    try {
+        var preparedArray;
+        if (!array || !length) {
+            throw("Exception for input data: length: " + length + " , array: " + array + ". length and elements must be integer numbers!");
+        } else if (array.length < length) {
+            throw("Exception for input data: length: " + length + " , array: " + array + ". Array must contain minimum " + length + " numbers.");
+        } else {
+            preparedArray = array.split(" ");
+            preparedArray = preparedArray.slice(0, length)
+            return preparedArray;
+        }
+    } catch (ex) {
+        console.error("Exception in prepareInputArrayForAlgorithm function.");
+        throw ex;
+    }
+}
+
+function getResultStringPartForIteration(iteration, result) {
+    var resultString = RESULT_BASIC_STRING;
+    var resultedString = result;
+    if (result === false) {
+        resultedString = "NO";
+    }
+    resultString = resultString + iteration + ": " + resultedString + "\n";
+    return resultString;
+}
+
+
+var sourceFileString = readFromFile(DEFAULT_FILE_IN_PATH, ENCODING);
+var inputStrings = sourceFileString.split("\n");
+inputStrings = preparationOfInputData(inputStrings);
+var algorithmIterationCount = parseInt(inputStrings[NUMBER_OF_TESTS_STRING_INDEX], 10);
+if (isNaN(algorithmIterationCount)) {
+    throw("First string in input file must be number!")
+}
+var linesWithTests = algorithmIterationCount * LINES_FOR_ONE_TEST_CASE;
+var resultString = "";
+
+/*
+    Processing each set of data and write them into the output string
+ */
+var algorithm = new greedyMishaAlgorithm.GreedyMishaAlgorithm();
+for (var j = STARTED_OF_TESTS_STRING_INDEX; j < linesWithTests; j++) {
+    var getIterationNumber = function (number) {
+        return (number + 1) / 2;
+    };
+    try {
+        // console.log("предобработка массива номер " + ((j + 1) / 2) + ". А именно: " + inputStrings[j + 1]);
+        if (j+1 > linesWithTests) {
+            throw("After all strings with elements count, must follow string with elements.")
+        } else {
+            var algorithmInputArray = prepareInputArrayForAlgorithm(inputStrings[j], inputStrings[j + 1]);
+            resultString += getResultStringPartForIteration(getIterationNumber(j), algorithm.start(algorithmInputArray));
+        }
+    } catch (ex) {
+        // Можно заменить на throw, если будет встраиваться в другую систему.
+        console.error("Error in test case with number: " + ((j + 1) / 2) + ". Exception: " + ex)
+    }
+    j++
+}
+
+writeToFile(resultString, DEFAULT_FILE_OUT_PATH);
+
+
+
